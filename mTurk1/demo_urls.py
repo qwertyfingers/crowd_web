a42ed4c85334cf2d715b652f97f7863ab91ec591
Barry Park <barry.park1@hotmail.com>
2013-01-20 12:46:41 +0000

    initial commit
    Changed url patterns to use named url groups

--- mTurk1/demo_urls.py
+++ mTurk1/demo_urls.py
@@ -5,6 +5,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('mTurk1.views',
                        url(r'^display_keys/$', 'display_keys',{'cur_app':'mturk1_demo'}, name="display_keys",),
-                       url(r'^([A-Z0-9]{30})/$', 'simulation',{'cur_app':'mturk1_demo'}, name='simulation'),
-                       url(r'^([A-Z0-9]{30})([A-Z0-9]{30})/thankyou/$', 'thankyou_post',{'cur_app':'mturk1_demo'},  name='thankyou_post')
+                       url(r'^(?P<key>[A-Z0-9]{30})/$', 'simulation',{'cur_app':'mturk1_demo'}, name='simulation'),
+                       url(r'^(?P<key>[A-Z0-9]{30})(?P<reward_key>[A-Z0-9]{30})/thankyou/$', 'thankyou_post',{'cur_app':'mturk1_demo'},  name='thankyou_post')
                        )
