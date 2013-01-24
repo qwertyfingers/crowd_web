8d05ec02e8c9012f8c7adc16d280db92a7408b4d
Barry Park <barry.park1@hotmail.com>
2013-01-24 11:38:30 +0000

    tutorial exp

---
+++ mTurk1/simulations/sim_test_1/configExp_tutorial.py
@@ -0,0 +1,25 @@
+import os
+from mTurk1.python.database_functions import sim_path, agent_path, view_path
+
+#This is the tutorial for experiment_1
+
+class ExperimentSettings:
+    
+    #Name the simulation
+    sim_name='Simulation_0.1'
+    #Name the experiment
+    exp_name='experiment_1_tut'
+
+    #Setup folder location
+    setup_folder=os.path.abspath(os.path.join(os.path.dirname( __file__ ),'setup_files'))
+
+    sim1=sim_path(setup_folder,"sim_settings_test_0.1.json")
+    agent1=agent_path(setup_folder,"agent_settings_test_0.1.json")
+    view1=view_path(setup_folder,"view_settings_test_0.1.json")
+
+    
+    sim_list=[sim1]
+    agent_list=[agent1]
+    view_list=[view1]
+    
+    
